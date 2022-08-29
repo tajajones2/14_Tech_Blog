009343b7bac6a33e038d90b5dcc5c548e6ce4263
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-29 19:29:35 -0400

    created commennt js file and started blog js file

---
+++ models/comment.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require("sequelize")
+
+
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        blog_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+
+    }
+)
