1dd828913574054599b371f0eb1b8fd206409065
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-30 22:10:21 -0400

    formatted commentData blogData userData

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -1,12 +1,14 @@
-const {Blog} = require ("../models")
+const { Blog } = require("../models");
 
-const blogData = [{
-    description: "MVC allows developers to maintain a true seperation of concerns, devising their code between the model layer for data, the view layer for design and controller layer for application logic.",
+const blogData = [
+  {
+    description:
+      "MVC allows developers to maintain a true seperation of concerns, devising their code between the model layer for data, the view layer for design and controller layer for application logic.",
     title: "Why is so important",
     date: "08/28/2022",
     user_id: "1",
-
-}];
+  },
+];
 
 const blogs = () => Blog.bulkCreate(blogData);
 
--- seeds/userData.js
+++ seeds/userData.js
@@ -1,15 +1,16 @@
-const {User} = require('../models');
+const { User } = require("../models");
 
-const userData = [{
-    username: 'Taja',
-    password: 'password',
-}]; 
+const userData = [
+  {
+    username: "Taja",
+    password: "password",
+  },
+];
 
-const users = () => User.bulkCreate (userData, {
+const users = () =>
+  User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
-});
+  });
 
 module.exports = users;
-
-
