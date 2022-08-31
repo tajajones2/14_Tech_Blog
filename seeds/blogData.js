62d480c340d385446f294746cc2d822b5fe68b6f
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-30 23:44:50 -0400

    added app dot use routes on server js && created homeroutes under api controllers folder && started creating the start of route on index js file under controllers folder also switched title list on blogData js file

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -2,9 +2,9 @@ const { Blog } = require("../models");
 
 const blogData = [
   {
+    title: "Why is so important",
     description:
       "MVC allows developers to maintain a true seperation of concerns, devising their code between the model layer for data, the view layer for design and controller layer for application logic.",
-    title: "Why is so important",
     date: "08/28/2022",
     user_id: "1",
   },
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const express = require('express')
+const routes = require('./controllers')
 
 const sequelize = require("./config/connection")
 
@@ -12,6 +13,10 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.json)
 app.use(express.urlencoded({extended:true}))
+
+
+// this is for using routes
+app.use(routes);
 
 
 sequelize.sync({force:false}).then(()=>{
