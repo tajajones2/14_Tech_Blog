0f2497a514be4f053cd0c96f7ef50edcb0071e0e
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-30 22:27:20 -0400

    created seedDatabase function on index js file under seeds folder

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,21 @@
 const sequelize = require ('../config/connection');
-const {User} = require ('../models');
 
 
+const users = require ('./userData');
+const blogs = require ('./blogData')
+const comments = require ('./commentData')
 
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true})
+
+    await users();
+
+    await blogs();
+
+    await comments();
+
+    process.exit(0);
+
+};
+
+seedDatabase();
