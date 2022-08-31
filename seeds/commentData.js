f4ab8017e67e452167933d201828f7c1c2180a6f
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-30 22:03:31 -0400

    created blogData commentData seeds

---
+++ seeds/commentData.js
@@ -0,0 +1,14 @@
+const {Comment} = require('../models');
+
+const commentData = [{
+    description: "This is so cool!",
+    date: "08/28/2022",
+    user_id: "1",
+    blog_id: "1",
+
+}];
+
+const comments = () => Comment.bulkCreate (commentData);
+
+module.exports = comments;
+
