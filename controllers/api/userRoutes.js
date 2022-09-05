d6c8ce2859acff66b4444f0d92b042ccc0535d3f
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-09-05 03:12:57 -0400

    changed const for userRoutes to call to appropiate file && create router for homepage && added export for userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -57,3 +57,5 @@ router.post('/logout', (req, res) => {
 
 
 });
+
+module.exports = router
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,16 +4,54 @@ const { User, Blog, Comment } = require("../models");
 router.get("/", async (req, res) => {
   try {
     // get all user data
-    const allUsers = await User.findAll({
-        include: []
+    const blogData = await Blog.findAll({
+        include: [{
+          model: User,
+        }]
     });
 
-    res.send("hello world!")
+  //serialize data so the template can read it
+const blogs = blogData.map((project) => project.get({ plain: true }));
+
+    // res.send("homepage",)
+    res.json(blogs)
+
+
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
+// router.get ("/blog", async (req, res) => {
+//   if ()
+//   try { const blogData = await Blog.findAll({
+//     where: { user_id: },
+//     include: [
+//       model: User
+//     ]
+//   })
+
+//   };
+// })
+
+
+
+
+
+
+
+
 module.exports = router;
 
+// router.get("/", async (req, res) => {
+//   try {
+//     // get all user data
+//     const allUsers = await User.findAll({
+//         include: []
+//     });
 
+//     res.send("hello world!")
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
