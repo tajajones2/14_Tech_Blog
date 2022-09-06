b7cb381ff98723d3f997a262062e6022f2b6f262
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-09-05 23:08:35 -0400

    completed retrevial of user data for blog and comment and exculed the show of password

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,9 +5,11 @@ router.get("/", async (req, res) => {
   try {
     // get all user data
     const blogData = await Blog.findAll({
-      include: [{
-          model: User
-        }]
+      include: [
+        {
+          model: User,
+        },
+      ],
     });
 
     //serialize data so the template can read it
@@ -20,36 +22,31 @@ router.get("/", async (req, res) => {
   }
 });
 
-
 router.get("/blog/:id", async (req, res) => {
   try {
-    const blogData = await Blog.findOne({
-      where: [{
-          id: req.params.id
-        }]
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+          { model: User, attributes: {exclude: ["password"] } },
+        // gets user data for blog and user data for comment
+        
+          { model: Comment,  include: [
+            { model: User, attributes: { exclude: ["password"] } }
+      ]},
+      ],
     })
+
     // const blogs = blogData.map((project) => project.get({ plain: true }));
 
     // this is to send data back
-    res.status(200).json(blogData)
+    res.status(200).json(blogData);
 
     ////////
   } catch (error) {
-    res.status(500). json(error);
+    res.status(500).json(error);
   }
 });
 
-
-
-
-
 module.exports = router;
-
-
-
-
-
-
 
 // router.get("/", async (req, res) => {
 //   try {
