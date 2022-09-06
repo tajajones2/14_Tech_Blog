18959796da833c642254db022e49067a15fddd5a
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-09-05 20:40:57 -0400

    commented out app dot use

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const routes = require('./controllers')
 const app = express() 
 const PORT = process.env.PORT || 3001
 
-app.use(express.json)
-app.use(express.urlencoded({extended:true}))
+// app.use(express.json)
+// app.use(express.urlencoded({extended:true}))
 
 
 // this is for using routes
